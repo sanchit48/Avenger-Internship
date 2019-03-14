e3f375b8c5c937834b530b244fb3896e4b541741
sanchit <sanchitkhare3@gmail.com>
2019-03-14 22:41:36 +0530

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $sudo
+     apt update
+
+
+  /*
+  var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+  */
+
+});
